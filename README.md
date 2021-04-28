4c3831ac1972790a6c5b3defb5cbc296774c8487
PechurinaLena <www.waaay.net@gmail.com>
2021-04-28 17:47:25 +0300

    feat: add new array with map function

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# js-tasks
+# Task 1
+ 1. Polyfill for map, filter and reduce functions
+
+ 2. Get a new array with map function like [{id: ..., title: ...},...]
+    [check here](https://codepen.io/pechurinalena/pen/oNBOmbN)
+    
+    
+    
---
+++ task1/newArr.js
@@ -0,0 +1,54 @@
+// polyfill for map function
+// if (!Array.prototype.map) {
+//   Array.prototype.map = function(cb) {
+//     let newArray = [];
+//     for(let item of this) {
+//       newArray.push(cb(item));
+//     }
+//     return newArray;
+//   }
+// }
+
+const notes = [
+  {
+    id: 1,
+    title: 'Recipe',
+    description: 'Ingredients include 2 eggs...',
+    pagesCount: 2,
+    isMarked: false,
+    access: []
+  },
+  {
+    id: 2,
+    title: 'Workouts',
+    description: '3 sets of squats...',
+    pagesCount: 1,
+    isMarked: true,
+    access: []
+  },
+  {
+    id: 3,
+    title: 'Passwords',
+    description: 'VISA ...',
+    pagesCount: 6,
+    isMarked: true,
+    access: []
+  },
+  {
+    id: 4,
+    title: 'To Do 2021',
+    description: '1. Learn JS...',
+    pagesCount: 3,
+    isMarked: false,
+    access: []
+  }
+]
+
+const newArray = notes.map(
+  obj => {
+    return {
+      id: obj.id,
+      title: obj.title
+    };
+  });
+console.log(newArray)
