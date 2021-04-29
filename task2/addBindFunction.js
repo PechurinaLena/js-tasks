295a205ed2a2dbf9a68b3ff528f19f416d532baf
PechurinaLena <www.waaay.net@gmail.com>
2021-04-29 12:41:36 +0300

    fix: change bind function

--- task2/addBindFunction.js
+++ task2/addBindFunction.js
@@ -9,12 +9,8 @@ testThis(100); // x=1, y=100
 const boundFunction = bind(testThis, context);
 boundFunction(100); // x=2, y=100
 
-function bind(fn, context) {
-  return function() {
-    const arr = [];
-    for (let i = 0; i < arguments.length; i++) {
-      arr.push(arguments[i]);
-    }
-    return fn.apply(context, arr);
-  }
+function bind(fn, context){
+  return function(){
+    fn.apply(context, arguments);
+  };
 }
