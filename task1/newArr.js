e0e981d18354723bacc3f8c163e5f3b146f9795c
PechurinaLena <www.waaay.net@gmail.com>
2021-05-04 10:28:01 +0300

    fix: code style fixes

--- task1/newArr.js
+++ task1/newArr.js
@@ -10,45 +10,42 @@
 // }
 
 const notes = [
-  {
-    id: 1,
-    title: 'Recipe',
-    description: 'Ingredients include 2 eggs...',
-    pagesCount: 2,
-    isMarked: false,
-    access: []
-  },
-  {
-    id: 2,
-    title: 'Workouts',
-    description: '3 sets of squats...',
-    pagesCount: 1,
-    isMarked: true,
-    access: []
-  },
-  {
-    id: 3,
-    title: 'Passwords',
-    description: 'VISA ...',
-    pagesCount: 6,
-    isMarked: true,
-    access: []
-  },
-  {
-    id: 4,
-    title: 'To Do 2021',
-    description: '1. Learn JS...',
-    pagesCount: 3,
-    isMarked: false,
-    access: []
-  }
+    {
+        id: 1,
+        title: 'Recipe',
+        description: 'Ingredients include 2 eggs...',
+        pagesCount: 2,
+        isMarked: false,
+        access: [],
+    },
+    {
+        id: 2,
+        title: 'Workouts',
+        description: '3 sets of squats...',
+        pagesCount: 1,
+        isMarked: true,
+        access: [],
+    },
+    {
+        id: 3,
+        title: 'Passwords',
+        description: 'VISA ...',
+        pagesCount: 6,
+        isMarked: true,
+        access: [],
+    },
+    {
+        id: 4,
+        title: 'To Do 2021',
+        description: '1. Learn JS...',
+        pagesCount: 3,
+        isMarked: false,
+        access: [],
+    },
 ]
 
-const newArray = notes.map(
-  note => {
-    return {
-      id: note.id,
-      title: note.title
-    };
-  });
+const newArray = notes.map((note) => ({
+    id: note.id,
+    title: note.title,
+}))
 console.log(newArray)
--- task1/reduceArr.js
+++ task1/reduceArr.js
@@ -1,38 +1,41 @@
 const notes = [
-  {
-    id: 1,
-    title: 'Recipe',
-    description: 'Ingredients include 2 eggs...',
-    pagesCount: 2,
-    isMarked: false,
-    access: []
-  },
-  {
-    id: 2,
-    title: 'Workouts',
-    description: '3 sets of squats...',
-    pagesCount: 1,
-    isMarked: true,
-    access: []
-  },
-  {
-    id: 3,
-    title: 'Passwords',
-    description: 'VISA ...',
-    pagesCount: 6,
-    isMarked: true,
-    access: []
-  },
-  {
-    id: 4,
-    title: 'To Do 2021',
-    description: '1. Learn JS...',
-    pagesCount: 3,
-    isMarked: false,
-    access: []
-  }
+    {
+        id: 1,
+        title: 'Recipe',
+        description: 'Ingredients include 2 eggs...',
+        pagesCount: 2,
+        isMarked: false,
+        access: [],
+    },
+    {
+        id: 2,
+        title: 'Workouts',
+        description: '3 sets of squats...',
+        pagesCount: 1,
+        isMarked: true,
+        access: [],
+    },
+    {
+        id: 3,
+        title: 'Passwords',
+        description: 'VISA ...',
+        pagesCount: 6,
+        isMarked: true,
+        access: [],
+    },
+    {
+        id: 4,
+        title: 'To Do 2021',
+        description: '1. Learn JS...',
+        pagesCount: 3,
+        isMarked: false,
+        access: [],
+    },
 ]
 
-let initialValue = 0
-const sumOfNotesPages = notes.reduce((sum, note) => sum + note.pagesCount, initialValue)
+const initialValue = 0
+const sumOfNotesPages = notes.reduce(
+    (sum, note) => sum + note.pagesCount,
+    initialValue
+)
 console.log(sumOfNotesPages)
