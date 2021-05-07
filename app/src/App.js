9215574e9d72b2966bcae3729bd8624fab93b406
PechurinaLena <www.waaay.net@gmail.com>
2021-05-07 16:29:34 +0300

    added additional files

---
+++ app/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./pages/home/home";
+import NotesContainer from "./pages/myNotes/NotesContainer";
+import styles from "./styles.module.css";
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <nav className={styles.mainNav}>
+          <ul className={styles.menu}>
+            <li className={styles.menuItem}>
+              <Link to="/" className={styles.menuLink}>
+                Home
+              </Link>
+            </li>
+            <li className={styles.menuItem}>
+              <Link to="/notes" className={styles.menuLink}>
+                Notes
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/notes" component={NotesContainer} />
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
